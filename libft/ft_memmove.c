bec1f9b335bd7a385c77d667a1a0050517fec0fa
Cristina Alarcon <cralarco@c1r5s1.42london.com>
2024-11-29 19:27:17 +0000

    improvements

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,33 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cralarco <cralarco@student.42london.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/29 11:09:58 by cralarco          #+#    #+#             */
-/*   Updated: 2024/11/29 18:00:51 by cralarco         ###   ########.fr       */
+/*   Created: 2024/11/29 12:23:59 by cralarco          #+#    #+#             */
+/*   Updated: 2024/11/29 19:24:51 by cralarco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <string.h>
 #include <stdio.h>
+#include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	int		i;
 	char	*buffer1;
 	char	*buffer2;
 
-	i = 0;
-	buffer1 = (void *)dest;
-	buffer2 = (void *)src;
-	while (i < n)
+	buffer1 = (void *)src;
+	buffer2 = (void *)dest;
+	if (buffer1 >= buffer2)
 	{
-		buffer1[i] = buffer2[i];
-		i++;
+		i = 0;
+		while (i < n)
+		{
+			buffer2[i] = buffer1[i];
+			i++;
+		}
 	}
-	buffer1[i] = 0;
-	return (buffer1);
+	else
+	{
+		i = ft_strlen(buffer1);
+		while (i > 0)
+		{
+			buffer2[i - 1] = buffer1[i - 1];
+			i--;
+		}
+	}
 }
 
 /*
@@ -35,7 +47,7 @@ void    main()
 {
         char src[5] = "abcde";
 
-        ft_memcpy(src, src + 2, 3);
+        ft_memmove(src, src + 2, 3);
         printf("%s\n", src);
 }
 */
